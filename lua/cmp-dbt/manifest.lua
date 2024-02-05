6a825cd30c7b67ee06a266be0ca9c40fb23d2cfd
MattiasMTS <mattias.sjodin@northvolt.com>
2024-02-05 22:29:21 +0100

    feat: set cron job for manifest checker

--- lua/cmp-dbt/manifest.lua
+++ lua/cmp-dbt/manifest.lua
@@ -1,4 +1,5 @@
 local Path = require("plenary.path")
+local context_manager = require("plenary.context_manager")
 
 -- ManifestLoader class definition
 local ManifestLoader = {}
@@ -7,6 +8,9 @@ local ManifestLoader = {}
 function ManifestLoader:new()
   local cls = {
     manifest_cache = {},
+    manifest_path = nil,
+    last_manifest_timestamp = nil,
+    interval_ms = 5000,
   }
   setmetatable(cls, self)
   self.__index = self
@@ -18,7 +22,7 @@ local function find_dbt_project()
   local current_dir = vim.fn.expand("%:p:h")
   local project_path = ""
 
-  -- TODO: replace / with os separator and dbt_project.yml with yaml/yml
+  -- TODO: support dbt_project.yaml and windows \\ paths
   while current_dir ~= "/" do
     project_path = Path:new(current_dir, "dbt_project.yml"):absolute()
     if vim.fn.filereadable(project_path) == 1 then
@@ -31,7 +35,7 @@ local function find_dbt_project()
   return project_path
 end
 
--- Private method to find the manifest.json within the target folder
+-- Find the manifest.json within the target folder
 local function find_manifest_json(project_path)
   local target_path = Path:new(Path:new(project_path):parent(), "target")
   local manifest_path = Path:new(target_path, "manifest.json"):absolute()
@@ -44,6 +48,7 @@ function ManifestLoader:load_manifest(callback)
   local project_path = find_dbt_project()
 
   if project_path == "" then
+    -- TODO: convert to debug logging
     vim.notify_once("dbt_project.yml not found!")
     return
   end
@@ -51,19 +56,48 @@ function ManifestLoader:load_manifest(callback)
   local manifest_path = find_manifest_json(project_path)
 
   if vim.fn.filereadable(manifest_path) ~= 1 then
+    -- TODO: convert to debug logging
     vim.notify_once("manifest.json not found in the target folder!")
     return
   end
 
-  if not self.manifest_cache[manifest_path] then
-    local ok, data = pcall(vim.fn.json_decode, vim.fn.readfile(manifest_path))
-    if ok then
+  if not self.manifest_cache[manifest_path] or self:manifest_updated(manifest_path) then
+    local with = context_manager.with
+    local open = context_manager.open
+
+    with(open(manifest_path, "r"), function(f)
+      local data = vim.fn.json_decode(f:read())
       self.manifest_cache[manifest_path] = data
+      self.manifest_path = manifest_path
       callback(data)
-    end
+    end)
   end
 
   callback(self.manifest_cache[manifest_path])
+end
+
+-- Check if the manifest file has been updated
+function ManifestLoader:manifest_updated(manifest_path)
+  local current_timestamp = vim.fn.getftime(manifest_path)
+  if not self.last_manifest_timestamp or current_timestamp > self.last_manifest_timestamp then
+    self.last_manifest_timestamp = current_timestamp
+    return true
+  end
+  return false
+end
+
+-- Poll the manifest.json for changes
+function ManifestLoader:cron_manifest(callback)
+  local timer = vim.loop.new_timer()
+  timer:start(
+    self.interval_ms,
+    self.interval_ms,
+    vim.schedule_wrap(function()
+      if self:manifest_updated(self.manifest_path) then
+        callback()
+      end
+    end)
+  )
 end
 
 return ManifestLoader
--- lua/cmp-dbt/source.lua
+++ lua/cmp-dbt/source.lua
@@ -1,41 +1,75 @@
 -- source is the source of the completion items.
 local source = {}
 
-local utils = require("cmp-dbt.utils")
+-- local utils = require("cmp-dbt.utils")
+local ma = require("cmp-dbt.manifest")
 
 function source:new()
   local cls = {
-    dbt = require("cmp-dbt.dbt"):new(),
+    dbt = ma:new(),
     manifest = {},
+    manifest_path = "",
   }
   setmetatable(cls, self)
   self.__index = self
 
-  cls:set_manifest_async()
+  -- setup cron job to check for manifest changes
+  cls.dbt:cron_manifest(function()
+    cls:handle_manifest_setter()
+  end)
+
   return cls
 end
 
-function source:set_manifest_async()
-  local timeout_ms = 1000
-  vim.defer_fn(function()
-    self.dbt:load_manifest(function(manifest)
-      self.manifest = manifest
-    end)
-  end, timeout_ms)
+function source:handle_manifest_setter()
+  self.dbt:load_manifest(function(manifest)
+    self.manifest = manifest
+    self.manifest_path = self.dbt.manifest_path
+  end)
 end
 
-function source:get_completion()
-  local cursor_before_line = utils:get_cursor_before_line()
+-- TODO: continue here to add good documentation for the completion items
+local function get_documentation(item)
+  local ok, parsed = pcall(vim.fn.json_encode, item)
+  if ok then
+    return parsed
+  end
+  return ""
+end
 
-  return {}
+local function convert_many_to_completion_item(items)
+  local out = {}
+  for k, v in pairs(items) do
+    -- extract the last part of the model (has to be unique)
+    k = k:match("([^.]*)$")
+    table.insert(out, {
+      label = k,
+      kind = vim.lsp.protocol.CompletionItemKind.Text,
+      documentation = get_documentation(v),
+    })
+  end
+  return out
+end
+
+-- TODO: continue here, add e.g. column completion, macros, test, etc.
+function source:get_completion()
+  local nodes = self.manifest.nodes or {}
+  return convert_many_to_completion_item(nodes)
 end
 
 function source:is_available()
-  return true
+  if self.manifest_path == "" then
+    self.dbt:load_manifest(function(manifest)
+      self.manifest = manifest
+      self.manifest_path = self.dbt.manifest_path
+    end)
+  end
+
+  return self.manifest_path ~= ""
 end
 
 function source:get_trigger_characters()
-  return { ".", "{", " " }
+  return { ".", "{", "(", " " }
 end
 
 function source:get_debug_name()
