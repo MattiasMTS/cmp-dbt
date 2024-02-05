c7a7fe5f935af33d9414d09965d4ea032ad00e8c
MattiasMTS <mattias.sjodin@northvolt.com>
2024-02-05 06:51:51 +0100

    feat: add source completion

--- lua/cmp-dbt/source.lua
+++ lua/cmp-dbt/source.lua
@@ -1,20 +1,41 @@
 -- source is the source of the completion items.
 local source = {}
 
+local utils = require("cmp-dbt.utils")
+
 function source:new()
-  local cls = {}
+  local cls = {
+    dbt = require("cmp-dbt.dbt"):new(),
+    manifest = {},
+  }
   setmetatable(cls, self)
   self.__index = self
+
+  cls:set_manifest_async()
   return cls
 end
 
--- TODO:
+function source:set_manifest_async()
+  local timeout_ms = 1000
+  vim.defer_fn(function()
+    self.dbt:load_manifest(function(manifest)
+      self.manifest = manifest
+    end)
+  end, timeout_ms)
+end
+
 function source:get_completion()
+  local cursor_before_line = utils:get_cursor_before_line()
+
   return {}
 end
 
+function source:is_available()
+  return true
+end
+
 function source:get_trigger_characters()
-  return { " " }
+  return { ".", "{", " " }
 end
 
 function source:get_debug_name()
