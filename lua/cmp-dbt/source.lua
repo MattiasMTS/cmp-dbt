8322d517ed8e908b7d3ae5fff2df0bb342c9a95f
MattiasMTS <mattias.sjodin@northvolt.com>
2024-02-10 21:53:45 +0100

    feat: add sources completion

--- lua/cmp-dbt/source.lua
+++ lua/cmp-dbt/source.lua
@@ -3,12 +3,14 @@ local source = {}
 
 -- local utils = require("cmp-dbt.utils")
 local ma = require("cmp-dbt.manifest")
+local utils = require("cmp-dbt.utils")
 
 function source:new()
   local cls = {
     dbt = ma:new(),
     manifest = {},
     manifest_path = "",
+    found_sources = {},
   }
   setmetatable(cls, self)
   self.__index = self
@@ -30,6 +32,10 @@ end
 
 -- TODO: continue here to add good documentation for the completion items
 local function get_documentation(item)
+  if not item then
+    return ""
+  end
+
   local ok, parsed = pcall(vim.fn.json_encode, item)
   if ok then
     return parsed
@@ -40,8 +46,6 @@ end
 local function convert_many_to_completion_item(items)
   local out = {}
   for k, v in pairs(items) do
-    -- extract the last part of the model (has to be unique)
-    k = k:match("([^.]*)$")
     table.insert(out, {
       label = '"' .. k .. '"',
       kind = vim.lsp.protocol.CompletionItemKind.Text,
@@ -54,7 +58,46 @@ end
 -- TODO: continue here, add e.g. column completion, macros, test, etc.
 function source:get_completion()
   local nodes = self.manifest.nodes or {}
+  local cursor_before_line = utils:get_cursor_before_line()
+
+  if cursor_before_line:match("source") then
+    local cmp_sources = self:completion_for_source(cursor_before_line)
+    return convert_many_to_completion_item(cmp_sources)
+  end
+
   return convert_many_to_completion_item(nodes)
+end
+
+function source:completion_for_source(line)
+  local sources = self.manifest.sources or {}
+  local cursor_before_line = line or utils:get_cursor_before_line()
+
+  -- if we are using e.g. {{ source('source_name', ) }} then we want
+  -- to see all the models for that source_name.
+  if self.found_sources then
+    for f, _ in pairs(self.found_sources) do
+      if cursor_before_line:match(f) then
+        local out = {}
+        for k, v in pairs(sources) do
+          if k:match(f) then
+            -- captures the last part of the string -> model_name
+            k = k:match("([^.]*)$")
+            out[k] = v
+          end
+        end
+        return out
+      end
+    end
+  end
+
+  -- if we are using e.g. {{ source('') }} then we want
+  -- to see all the sources.
+  for k, v in pairs(sources) do
+    -- captures the 2nd to last part of the string -> source_name
+    k = k:match("([^%.]+)%.[^%.]+$")
+    self.found_sources[k] = v
+  end
+  return self.found_sources
 end
 
 function source:is_available()
